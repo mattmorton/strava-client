21bc5fcd3213df6dd48aa3a55b4be218c0881017
Matt Morton <matthew.morton@leapdev.io>
2022-03-09 21:31:41 +1100

    add env files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  stravaBaseUrl: 'https://www.strava.com/api/v3'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  stravaBaseUrl: 'https://www.strava.com/api/v3'
+}
